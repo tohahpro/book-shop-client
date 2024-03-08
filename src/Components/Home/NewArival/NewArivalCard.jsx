bd641869148f852ece3320c31dcecc70f10abf66
Toha Hossain <tohahpro@gmail.com>
2024-03-08 11:04:28 +0600

    new arrival section created

--- src/Components/Home/NewArival/NewArivalCard.jsx
+++ src/Components/Home/NewArival/NewArivalCard.jsx
@@ -1,26 +1,32 @@
 import { Rating } from "@smastrom/react-rating";
 import '@smastrom/react-rating/style.css'
 
-const NewArivalCard = () => {
-    return (
-        <div className="flex">
+const NewArivalCard = ({ data }) => {
 
-            <div className="group relative items-center justify-center overflow-hidden cursor-pointer w-80">
-                <div className=" w-fit">
-                    <img className="h-full w-full object-cover" src="" alt="" />
+    const { bookName, writer, image, price, rating } = data
+
+    console.log(data);
+
+    return (
+        <div className="flex shadow-2xl rounded-lg p-4">
+
+            <div className="group relative items-center justify-center overflow-hidden cursor-pointer">
+                <div className="w-1/2">
+                    <img className="h-full w-full object-cover" src={image} alt="" />
                 </div>
-                {/* <div className="absolute inset-0 bg-gradient-to-b from-transparent group-hover:from-black/60 group-hover:to-black/60 group-hover:vai-black/60 group-hover:duration-700"></div> */}
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent group-hover:from-black/60 group-hover:to-black/60 group-hover:vai-black/60 group-hover:duration-700"></div>
                 <div className="absolute inset-0 flex flex-col items-center px-9 text-center translate-y-[110%] group-hover:translate-y-[70%] transition-all group-hover:duration-500">
                     <button className="px-3 py-2 bg-white hover:bg-[#F44344] hover:text-white text-base font-medium rounded-3xl">Add To Cart</button>
                 </div>
             </div>
             <div>
-                <h4 className="text-center pt-2 pb-1">dfgdf</h4>
-                <p className="text-center pb-1">By :df</p>
+                <h4 className="text-center pt-2 pb-1">{bookName}</h4>
+                <p className="text-center pb-1">{writer}</p>
+                <p className="text-center pb-1">{price}</p>
                 <div className="flex justify-center">
                     <Rating
                         style={{ maxWidth: 90 }}
-                        value="" readOnly
+                        value={rating} readOnly
                     />
                 </div>
             </div>
--- src/Components/Home/NewArival/NewArrival.jsx
+++ src/Components/Home/NewArival/NewArrival.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from 'react';
 import photo from '../../../assets/images/New-Arrival.png'
+import NewArivalCard from './NewArivalCard';
 
 const NewArrival = () => {
+
+    const [data, setData] = useState([])
+
+
+    useEffect(() => {
+        fetch('NewArrival.json')
+            .then(res => res.json())
+            .then(data => setData(data))
+    }, [])
+
     return (
-        <div>
-            <div>
-                <div>
+        <div className='py-32'>
+            <h3 className='text-center text-3xl font-bold py-10'>NEW ARRIVAL</h3>
+            <div className='grid grid-cols-12'>
+                <div className='col-span-4'>
                     <img src={photo} alt="" />
+                    <div>
+                        <p className='text-3xl font-bold text-center pb-4'>Photo Realistic</p>
+                        <div className='flex justify-center'>
+                            <button className='px-8 py-2 bg-black text-white text-lg font-semibold rounded-3xl hover:shadow-2xl hover:shadow-black hover:scale-110 hover:duration-700'>Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-span-7 grid grid-cols-2 gap-10'>
+
+                    {
+                        data.map((item, idx) =>
+                            <NewArivalCard key={idx} data={item} />
+                        )
+                    }
                 </div>
             </div>
         </div>
