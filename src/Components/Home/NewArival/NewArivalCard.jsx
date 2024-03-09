44d7bfda91980a28e65c89bf87e88d074c66c70a
Toha Hossain <tohahpro@gmail.com>
2024-03-09 09:37:54 +0600

    new arrival section working

--- src/Components/Home/NewArival/NewArivalCard.jsx
+++ src/Components/Home/NewArival/NewArivalCard.jsx
@@ -11,19 +11,19 @@ const NewArivalCard = ({ data }) => {
         <div className="flex shadow-2xl rounded-lg p-4">
 
             <div className="group relative items-center justify-center overflow-hidden cursor-pointer">
-                <div className="w-1/2">
-                    <img className="h-full w-full object-cover" src={image} alt="" />
+                <div className="w-full">
+                    <img className=" object-cover" src={image} alt="" />
                 </div>
                 <div className="absolute inset-0 bg-gradient-to-b from-transparent group-hover:from-black/60 group-hover:to-black/60 group-hover:vai-black/60 group-hover:duration-700"></div>
                 <div className="absolute inset-0 flex flex-col items-center px-9 text-center translate-y-[110%] group-hover:translate-y-[70%] transition-all group-hover:duration-500">
                     <button className="px-3 py-2 bg-white hover:bg-[#F44344] hover:text-white text-base font-medium rounded-3xl">Add To Cart</button>
                 </div>
             </div>
-            <div>
-                <h4 className="text-center pt-2 pb-1">{bookName}</h4>
-                <p className="text-center pb-1">{writer}</p>
-                <p className="text-center pb-1">{price}</p>
-                <div className="flex justify-center">
+            <div className="w-full pl-5">
+                <h4 className="pt-2 pb-1 font-semibold">{bookName}</h4>
+                <p className="pb-1">Dr. {writer}</p>
+                <p className="pb-1">$ {price}</p>
+                <div className="">
                     <Rating
                         style={{ maxWidth: 90 }}
                         value={rating} readOnly
